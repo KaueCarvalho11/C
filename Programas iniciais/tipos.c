00d3d91f59a2b3513306f9979c964d4a9a661584
Kauê Carvalho <kauecarvalho2028@gmail.com>
2024-05-31 20:05:36 -0300

    Lidando com variáveis

---
+++ Programas iniciais/tipos.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int idade = 19;
+    float dinheiro = 50.12;
+    double particula = 2.543934823040234;
+    char tipo = 'A';
+
+    printf("Idade: %d\n", idade);
+    printf("Dinheiro: %f\n", dinheiro);
+    printf("Velocidade da particula: %lf\n", particula);
+    printf("Tipo de habilitacao: %c\n", tipo);
+
+    return 0;
+}
